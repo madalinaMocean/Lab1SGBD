9d68a681157959d0107f27c3f9fc97def03a0f0a
madalinaMocean <maria.madalina.mocean@stud.ubbcluj.ro>
2022-03-09 23:36:34 +0200

    First version

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace SGBDLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.HotelIdTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 35);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(432, 329);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(506, 122);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(390, 329);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(559, 490);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 3;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(559, 529);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(559, 572);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // HotelIdTextBox
+            // 
+            this.HotelIdTextBox.Location = new System.Drawing.Point(691, 490);
+            this.HotelIdTextBox.Name = "HotelIdTextBox";
+            this.HotelIdTextBox.Size = new System.Drawing.Size(100, 23);
+            this.HotelIdTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 628);
+            this.Controls.Add(this.HotelIdTextBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.TextBox HotelIdTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGBDLab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            insertButton.Enabled = false;
+            updateButton.Enabled = false;
+            deleteButton.Enabled = false;
+           
+        }
+
+        string connectionString = "Data Source=DESKTOP-UV4O3H6\\SQLEXPRESS;Initial Catalog=HotelManagement;Integrated Security=True";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter;
+
+            string hotelQuery = "select * from Hotel";
+            string employeeQuery = "select * from Employee";
+            //create a DataSet
+            DataSet ds1 = new DataSet();
+
+            try
+            {
+                //open connection and create DataAdapter
+                connection.Open();
+                adapter = new SqlDataAdapter(hotelQuery, connection);
+                adapter.Fill(ds1,"Hotel");
+                adapter = new SqlDataAdapter(employeeQuery, connection);
+                adapter.Fill(ds1,"Employee");
+
+                ds1.Relations.Add("Hotel Employee",
+                ds1.Tables["Hotel"].Columns["HotelId"],
+                ds1.Tables["Employee"].Columns["HotelId"]);
+                connection.Close();
+
+                dataGridView1.DataSource = ds1.Tables["Hotel"];
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                connection.Close();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int iRownr = this.dataGridView1.CurrentCell.RowIndex;
+
+            object cellvalue1 = this.dataGridView1[0, iRownr].Value;
+            string IdString = cellvalue1.ToString();
+            int Id = Int32.Parse(IdString);
+            HotelIdTextBox.Text = IdString;
+            insertButton.Enabled = true;
+            updateButton.Enabled = false;
+            deleteButton.Enabled = false;
+            HotelIdTextBox.ReadOnly = true;
+
+            //create connection
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            //create the command and parameter objects
+            string childQuery = "select * from Employee where HotelId = @HotelId";
+            SqlCommand command = new SqlCommand(childQuery, connection);
+            command.Parameters.Add("@HotelId",SqlDbType.Int);
+            command.Parameters["@HotelId"].Value=Id;
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable childTable = new DataTable();
+                childTable.Load(reader);
+                dataGridView2.DataSource = childTable;
+                reader.Close();
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                connection.Close();
+            }
+
+
+        }
+    }
+}
